6a82a9adfdd432aef188ad0e41779536993fda93
Cuauhtemoc Herrera Muñoz <seifer.ch@gmail.com>
2019-09-01 22:49:41 -0500

    Added events

--- keeper.py
+++ keeper.py
@@ -3,11 +3,13 @@
 
 from string import ascii_lowercase, digits, punctuation
 # from shutil import copyfile
+from datetime import date
 from hashlib import md5
 from random import choice
 from tkinter import Listbox, Tk, Frame, Text, Toplevel, Label, ttk
-from tkinter import GROOVE, BOTH, LEFT, RIGHT, END, WORD
-from tkinter import NONE, FALSE
+from tkinter import Button, StringVar
+from tkinter import GROOVE, BOTH, LEFT, RIGHT, END, WORD, BOTTOM, X
+from tkinter import NONE, FALSE, TOP
 
 
 SYS_FONT = ('Consolas', '10')
@@ -29,41 +31,35 @@ class Note:
         self.date = ''
         self.content = ''
 
-    def serialize(self):
-        return self.id + '~' + self.title + '~' + self.date + '~' + self.content
-
-    def deserialize(self, string):
-        content = string.split('~')
-        self.id = content[0]
-        self.title = content[1]
-        self.date = content[2]
-        self.content = content[3]
-
 
 class UI:
 
     def __init__(self):
         self.notes = []
-        self.load_notes_from_files()
         self.init_ui()
 
-    def handle_note_select(self, event):
-        if(self.listbox.size() > 0):
-            index = int(event.widget.curselection()[0])
-            print(index)
-            self.textbox.delete('0.0', END)
-            self.textbox.insert('0.0', self.tasks[index-1].content)
+    def add_note(self):
+        note = Note()
+        note.id = get_md5_hash(get_random_string(4))
+        note.title = ''
+        note.date = date.today()
+        note.content = ''
+        self.notes.append(note)
+        self.listbox.insert(END, note.id + ' ' + str(note.date))
+        self.listbox.selection_clear(0, END)
+        self.listbox.select_set(END)
+        self.listbox.event_generate("<<ListboxSelect>>")
 
-    def popup_test(self, event):
-        print('something')
-        win = Toplevel()
-        win.wm_title("Window")
+    def select_note(self, event):
+        note = self.notes[self.listbox.curselection()[0]]
+        self.textbox_label_var.set(note.id + ' ' + str(note.date) + ' ' + note.title)
+        self.textbox.delete('0.0', END)
+        self.textbox.insert('0.0', note.content)
 
-        lx = Label(win, text="Input")
-        lx.grid(row=0, column=0)
-
-        b = ttk.Button(win, text="Okay", command=win.destroy)
-        b.grid(row=1, column=0)
+    def update_note_content(self, event):
+        if self.notes:
+            note = self.notes[self.listbox.curselection()[0]]
+            note.content = self.textbox.get('0.0', END)
 
     def init_ui(self):
         self.window = Tk()
@@ -73,11 +69,16 @@ class UI:
         self.container = Frame(self.window)
         self.container.configure(bg='#33312e')
 
-        self.listbox = Listbox(self.container)
+        self.note_content = Frame(self.container)
+        self.note_content.configure(bg='#33312e')
 
+        self.notes_panel = Frame(self.container)
+        self.notes_panel.configure(bg='#33312e')
+
+        self.listbox = Listbox(self.notes_panel)
         self.listbox.configure(font=SYS_FONT)
         self.listbox.configure(height=20)
-        self.listbox.configure(width=25)
+        self.listbox.configure(width=30)
         self.listbox.configure(bg='#33312e')
         self.listbox.configure(fg='#aeaaa3')
         self.listbox.configure(selectbackground='#353844')
@@ -85,32 +86,42 @@ class UI:
         self.listbox.configure(selectforeground='#b7c3db')
         self.listbox.configure(exportselection=FALSE)
         self.listbox.configure(activestyle=NONE)
+        self.listbox.bind('<<ListboxSelect>>', self.select_note)
 
-        self.listbox.bind('<<ListboxSelect>>', self.handle_note_select)
+        self.textbox_label = Label(self.note_content)
+        self.textbox_label_var = StringVar()
+        self.textbox_label.configure(textvariable=self.textbox_label_var)
+        self.textbox_label.configure(font=SYS_FONT)
+        self.textbox_label.configure(bg='#f5e6c6')
+        self.textbox_label.configure(fg='#494642')
 
-        self.textbox = Text(self.container)
-
+        self.textbox = Text(self.note_content)
         self.textbox.configure(font=SYS_FONT)
         self.textbox.configure(height=20)
-        self.textbox.configure(width=75)
+        self.textbox.configure(width=90)
         self.textbox.configure(bg='#f5e6c6')
         self.textbox.configure(fg='#494642')
         self.textbox.configure(insertbackground='#494642')
         self.textbox.configure(relief=GROOVE)
         self.textbox.configure(spacing1=1)
         self.textbox.configure(wrap=WORD)
+        self.textbox.bind('<KeyPress>', self.update_note_content)
 
-        self.textbox.bind('<KeyPress>', self.popup_test)
+        self.button_add_note = Button(self.notes_panel)
+        self.button_add_note.configure(text='+')
+        self.button_add_note.configure(command=self.add_note)
 
-        self.listbox.pack(side=LEFT, padx=10)
-        self.textbox.pack(side=RIGHT)
+        self.textbox_label.pack(side=TOP, fill=X, pady=5)
+        self.textbox.pack()
 
+        self.listbox.pack(padx=10)
+        self.button_add_note.pack(side=BOTTOM, fill=X, padx=10, pady=5)
+
+        self.notes_panel.pack(side=LEFT)
+        self.note_content.pack()
         self.container.pack(padx=20, pady=20, fill=BOTH)
 
         self.window.mainloop()
-
-    def load_notes_from_files(self):
-        pass
 
 
 ui = UI()
